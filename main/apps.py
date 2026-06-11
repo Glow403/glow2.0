e26f25e1ab019105a2bd124a6a7a71f983c8f99f
Glow403 <1704564243@qq.com>
2026-06-11 20:37:57 +0800

    Add files via upload

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,12 @@
-from django.apps import AppConfig
+﻿from django.apps import AppConfig
 
 
 class MainConfig(AppConfig):
     name = 'main'
+
+    def ready(self):
+        try:
+            from django.core.management import call_command
+            call_command('migrate', '--run-syncdb', verbosity=0)
+        except Exception:
+            pass
