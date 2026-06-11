f2e84973baebca2308b848606a2eb8e3ca03c4bd
Glow403 <1704564243@qq.com>
2026-06-11 21:40:25 +0800

    Add files via upload

--- main/apps.py
+++ main/apps.py
@@ -5,8 +5,4 @@ class MainConfig(AppConfig):
     name = 'main'
 
     def ready(self):
-        try:
-            from django.core.management import call_command
-            call_command('migrate', '--run-syncdb', verbosity=0)
-        except Exception:
-            pass
+        pass
--- website/wsgi.py
+++ website/wsgi.py
@@ -1,28 +1,16 @@
-"""
-WSGI config for website project.
+﻿import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
-It exposes the WSGI callable as a module-level variable named `application`.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
-import os
-import sys
-
-def apply_migrations():
-    """?????????????????????"""
+def setup():
+    import django
+    django.setup()
     try:
-        import django
-        django.setup()
         from django.core.management import call_command
-        call_command('migrate', '--run-syncdb', verbosity=0, stdout=open(os.devnull, 'w'))
+        call_command('migrate', '--run-syncdb', verbosity=0)
     except Exception:
         pass
 
-apply_migrations()
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
+setup()
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
